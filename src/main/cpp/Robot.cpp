2ca10febb9fc1ce214d875393bd33a37abc289cd
Gavo9649 <80174389+Gavo9649@users.noreply.github.com>
2022-11-15 11:45:19 -0500

    add a check if the bounding box is rectangular
    Limelight should not follow the tape unless the box's horizontal length is greater than its vertical length

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -550,14 +550,18 @@ void Robot::TeleopPeriodic() {
   double ty = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("ty", 0.0);
   double ta = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("ta", 0.0);
   double tv = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("tv", 0.0);
+  double thor = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("thor", 0.0);  
+  double tvert = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("tvert", 0.0);
   //Turn & Driving Tracking
-  if (tv > 0){ //if limelight has target (may break things but we'll see lol)
+  if (thor > tvert){ //if target bounding box is facing the right way
     
     if(tx < -8){ //target is too far left (need to turn right (positive))
       tx = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("tx", 0.0);
       ty = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("ty", 0.0);
       ta = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("ta", 0.0);
-
+      thor = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("thor", 0.0);  
+      tvert = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("tvert", 0.0);
+      
       LLSteerAdjust = tx*-.02; //target x value times -.02 (makes a positive turn value)
       m_drive.CurvatureDrive(0, LLSteerAdjust, true);
       
@@ -565,7 +569,9 @@ void Robot::TeleopPeriodic() {
       tx = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("tx", 0.0);
       ty = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("ty", 0.0);
       ta = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("ta", 0.0);
-
+      thor = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("thor", 0.0);  
+      tvert = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("tvert", 0.0);
+      
       LLSteerAdjust = tx*-.02; 
       m_drive.CurvatureDrive(0, LLSteerAdjust, true);
       
@@ -573,6 +579,8 @@ void Robot::TeleopPeriodic() {
       tx = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("tx", 0.0);
       ty = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("ty", 0.0);
       ta = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("ta", 0.0);
+      thor = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("thor", 0.0);  
+      tvert = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("tvert", 0.0);
       
       LLSteerAdjust = 0;
       m_drive.CurvatureDrive(0, LLSteerAdjust, true);
@@ -581,6 +589,8 @@ void Robot::TeleopPeriodic() {
       tx = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("tx", 0.0);
       ty = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("ty", 0.0);
       ta = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("ta", 0.0);
+      thor = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("thor", 0.0);  
+      tvert = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("tvert", 0.0);
       
       m_drive.CurvatureDrive(-.15, LLSteerAdjust, true); //go forwards
       
@@ -588,6 +598,8 @@ void Robot::TeleopPeriodic() {
       tx = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("tx", 0.0);
       ty = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("ty", 0.0);
       ta = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("ta", 0.0);
+      thor = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("thor", 0.0);  
+      tvert = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("tvert", 0.0);
       
       m_drive.CurvatureDrive(.15, LLSteerAdjust, true); //go backwards
       
