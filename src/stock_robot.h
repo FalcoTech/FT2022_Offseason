8e62c6bff5ef0e88b35422e6ad426a1d676bf8db
Gavo9649 <80174389+Gavo9649@users.noreply.github.com>
2023-01-04 15:08:47 -0500

    Update stock_robot.h

--- src/stock_robot.h
+++ src/stock_robot.h
@@ -1,1 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
+#pragma once
+
+#include <string>
+
+#include <frc/TimedRobot.h>
+#include <frc/smartdashboard/SendableChooser.h>
+#include "frc/AddressableLED.h"
+
+class Robot : public frc::TimedRobot {
+ public:
+ //any void functions go in here. Override FMS functions 
+ static constexpr int kLength = 180;
+  
+  void RobotInit() override;
+  void RobotPeriodic() override;
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+  void DisabledInit() override;
+  void DisabledPeriodic() override;
+  void TestInit() override;
+  void TestPeriodic() override;
+
+  void MoveClimber(double rotations);
+  //LEDS:
+
+    // PWM port 9
+  // Must be a PWM header, not MXP or DIO
+  frc::AddressableLED m_led{9};
+  std::array<frc::AddressableLED::LEDData, kLength>
+      m_ledBuffer;  // Reuse the buffer
+  // Store what the last hue of the first pixel is
+  int firstPixelHue = 0;
+   void Rainbow();
+  //  void Partymodeyeahwoo();
+ private:
+  frc::SendableChooser<std::string> m_chooser;
+  const std::string kAutoNameDefault = "Default";
+  const std::string kAutoNameCustom = "My Auto";
+  std::string m_autoSelected;
+
+  // frc::SendableChooser<std::string> m_chooser2;
+  // const std::string kDriveNameDefault = "Stevie";
+  // const std::string kDriveNameCustom = "Gavin";
+  // std::string m_drivestyleSelected;
+  
